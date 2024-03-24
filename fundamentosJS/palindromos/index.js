e12773f5e88d4a6b063f2ae05e9c1fc4c3427c23
Eduardo Farias Rizk <eduardo.rizk@sou.inteli.edu.br>
2024-03-24 12:28:04 -0300

    alt

---
+++ fundamentosJS/palindromos/index.js
@@ -0,0 +1,14 @@
+palíndromo e mostrar as mensagens de acordo:
+
+const palavra = prompt("Informe uma palavra:")
+let palavraInvertida = ""
+
+for (let i = palavra.length - 1; i >= 0; i--) {
+  palavraInvertida += palavra[i]
+}
+
+if (palavra === palavraInvertida) {
+  alert(palavra + " é um palíndromo!")
+} else {
+  alert(palavra + " não é um palíndromo!\n\n" + palavra + " !== " + palavraInvertida)
+}
--- teste.py
+++ teste.py
@@ -1,120 +1,60 @@
-# def soma_multiplos(n1, n2):
-#     i = 1
-#     soma = 0
-#     lim = max(n1,n2) * 10
-#     a1 = 0
-#     a2 = 0
-#     lista = []
+# def quadradoPerfeito(matriz):
+#     somaLinha = 0
+#     somaColuna = 0
+#     while True:                                                                            
+#         for i in range(len(matriz)):
+#             somaLinha += matriz[i].sum()
+#             for j in range(len(matriz)):
 
-#     if n2 > n1:
-#         while a2 <lim and i <= 10:
-#             a2 = n2 * i
-#             if a2 < lim:
-#                 soma += a2
-#                 lista.append(a2)
-#             i += 1
-#         i = 1
-#         while a1 < lim:
-#             a1 = n1 * i
-#             if a1 < lim:
-#                 soma += a1
-#                 if a1 not in lista:
-#                     lista.append(a1)
-#             i += 1
-#     else:
-#         while a1 < lim and i <= 10:
-#             a1 = n1 * i
-#             if a1 < lim:
-#                 soma += a1
-#                 lista.append(a1)
-#             i += 1
-#         i = 1
-#         while a2 < lim:
-#             a2 = n2 * i
-#             if a2 < lim:
-#                 soma += a2
-#                 if a2 not in lista:
-#                     lista.append(a2)
-#             i += 1
-#     return sum(lista)
-        
+
+# matriz = [[0,0,0,0],
+#           [0,0,0,0],
+#           [0,0,0,0]
+#           [0,0,0,0]
+#           ]     
+
+
+def quadrado_magico_estruturado(matriz):
+    n = len(matriz)  # Obtém a dimensão da matriz
+    soma_referencia = sum(matriz[0])  # Define a soma de referência como a soma dos elementos da primeira linha
+
+    # Verifica a soma de cada linha
+    for i in range(n):
+        if sum(matriz[i]) != soma_referencia:
+            return False
+
+    # Verifica a soma de cada coluna
+    for j in range(n):
+        soma_coluna = 0
+        for i in range(n):
+            soma_coluna += matriz[i][j]
+        if soma_coluna != soma_referencia:
+            return False
+
+    # Verifica a soma da diagonal principal
+    soma_diagonal_principal = 0
+    for i in range(n):
+        soma_diagonal_principal += matriz[i][i]
+    if soma_diagonal_principal != soma_referencia:
+        return False
+
+    # Verifica a soma da diagonal secundária
+    soma_diagonal_secundaria = 0
+    for i in range(n):
+        soma_diagonal_secundaria += matriz[i][n-1-i]
+    if soma_diagonal_secundaria != soma_referencia:
+        return False
+
+    # Se todas as somas são iguais à soma de referência, é um quadrado mágico
+    return True
+
+# Testando a função revisada com o mesmo exemplo
+quadrado_magico_estruturado([[6, 1, 8], [7, 5, 3], [2, 9, 4]])
+
+[Pedro,Val,Rodri]
+for i,v enumerate(lista):
     
-# user = soma_multiplos(2,5)
-# print(user)
+
+    
 
 
-# def soma_multiplos(n1, n2):
-#     i = 1
-#     soma = 0
-#     lim = max(n1, n2) * 10
-#     a1 = 0
-#     a2 = 0
-#     lista = []
-
-#     if n2 > n1:
-#         while a2 < lim and i <= 10:
-#             a2 = n2 * i
-#             if a2 < lim:
-#                 if a2 not in lista:
-#                     soma += a2
-#                     lista.append(a2)
-#             i += 1
-#         i = 1
-#         while a1 < lim:
-#             a1 = n1 * i
-#             if a1 < lim and a1 not in lista:
-#                 soma += a1
-#                 lista.append(a1)
-#             i += 1
-#     else:
-#         while a1 < lim and i <= 10:
-#             a1 = n1 * i
-#             if a1 < lim:
-#                 if a1 not in lista:
-#                     soma += a1
-#                     lista.append(a1)
-#             i += 1
-#         i = 1
-#         while a2 < lim:
-#             a2 = n2 * i
-#             if a2 < lim and a2 not in lista:
-#                 soma += a2
-#                 lista.append(a2)
-#             i += 1
-
-#     return soma
-
-# user = soma_multiplos(7,4)
-# print(user)
-
-
-# def soma_multiplos(n1, n2):
-#     # Define o limite como 10 vezes o maior entre n1 e n2
-#     limite = 10 * max(n1, n2)
-
-#     # Cria um conjunto para armazenar todos os múltiplos únicos
-#     multiplos = set()
-
-#     # Adiciona todos os múltiplos de n1 e n2 ao conjunto
-#     for i in range(limite + 1):
-#         if i % n1 == 0 or i % n2 == 0:
-#             multiplos.add(i)
-
-#     # Retorna a soma dos múltiplos únicos
-#     return sum(multiplos)
-
-# # Exemplos de teste
-# print(soma_multiplos(2, 5))  # Deve retornar 775
-# print(soma_multiplos(7, 4))  # Deve retornar 913
-
-def soma_multiplos(n1, n2):
-    limite = 10 * max(n1, n2)  # Define o limite como 10 vezes o maior entre n1 e n2
-    multiplos = set()  # Cria um conjunto para armazenar todos os múltiplos únicos
-    i = 0  # Inicializa o contador
-
-    while i <= limite:
-        if i % n1 == 0 or i % n2 == 0:
-            multiplos.add(i)  # Adiciona o número ao conjunto se for múltiplo de n1 ou n2
-        i += 1  # Incrementa o contador
-
-    return sum(multiplos)  # Retorna a soma dos múltiplos únicos
