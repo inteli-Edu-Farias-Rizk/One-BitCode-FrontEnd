87eb4583e1b3474162df956298d055a24a059fcb
inteli-edu-Farias-rizk <eduardo.rizk@sou.inteli.edu.br>
2024-03-24 12:26:58 -0300

    alt

--- fundamentosJS/3.JS_Moderno/ex014/index.js
+++ fundamentosJS/3.JS_Moderno/ex014/index.js
@@ -1,4 +1,4 @@
-const func = (...a) =>{
+const Media = (...a) =>{
     const soma =  a.reduce((acum,num)=>{
         return acum + num;
     },0);
@@ -16,17 +16,28 @@ const MediaPonderada = (...b) =>{
         return total/totalPeso
 }
 const mediana = (...c) => {
-    if (c.length % 2 == 0){
-        //this is to get the index of the middle, if the legth 6 the index´s are 3 and 4 of the middle
-        let numMed1 = c.length/2
-        let numMed2 = numMed1 + 1
-        //vai pegar os numeros do meio do array e guardar em uma lista
-        const numMed = c.filter((num)=>{
-            if 
-        })
+    
+    const NovaLista = [...c]
+    NovaLista.sort((a,b) => a-b)
+    //this is to get the index of the middle, if the legth 6 the index´s are 3 and 4 of the middle
+    let numMed1 = Math.floor(c.length/2)
+    let numMed2 = numMed1 + 1
+    //vai pegar os numeros do meio do array e guardar em uma lista
+    if (c.length % 2 !== 0){
+        console.log(`A mediana da lista é ${numMed1}`)
     }else{
-
+        console.log(`A mediana dessa lista é ${(numMed1 + NumMed2)/2}`)
     }
+
+}
+
+const Moda = (...a) =>{
+    const quantities = a.map((num)=>{
+        return [
+            num, numbers.filter(n)
+        ]
+    })
+
 }
 const media = func(2,6,3,7,4);
 console.log(media);
