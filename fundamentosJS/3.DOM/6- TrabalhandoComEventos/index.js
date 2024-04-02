850c03f40dc55b4064aea6cd4fd646c3dd2d8ccb
inteli-edu-Farias-rizk <eduardo.rizk@sou.inteli.edu.br>
2024-04-02 11:29:55 -0300

    alt

---
+++ fundamentosJS/3.DOM/6- TrabalhandoComEventos/index.js
@@ -0,0 +1,5 @@
+const button = document.getElementById('register-button')
+//vai ficar esperando os elementos acontecerem 
+button.addEventListener('click', function(){
+    alert('Botao clicado')
+})
