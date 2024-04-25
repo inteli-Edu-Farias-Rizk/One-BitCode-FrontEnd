e58819be500f7d1088c58282f5d482ae57a616f1
inteli-edu-Farias-rizk <eduardo.rizk@sou.inteli.edu.br>
2024-04-25 09:31:39 -0300

    alt

---
+++ fundamentosJS/3.DOM/12-JogoDaVelha/index.js
@@ -0,0 +1,11 @@
+document.getElementById('Comecar').addEventListener('click', function(ev){
+    ev.preventDefault();
+    
+    var input1 = document.getElementById('Nome1');
+    var input2 = document.getElementById('Nome2');
+
+    input2.value = '';
+    input1.value = '';
+
+    const p = createElement('p')
+});
